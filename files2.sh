f5341758a0ef1d56266eb89b1b4c892cf9451f19
codergustacaste <codergustacaste@gmail.com>
2016-07-30 11:18:30 -0700

    Add files via upload

---
+++ files2.sh
@@ -0,0 +1,78 @@
+#! /bin/bash
+touch A2SET.LDP-RSVP
+cat A2SET.cfg | grep ldp > A2SET.LDP-RSVP
+cat A2SET.cfg | grep LDP >> A2SET.LDP-RSVP
+cat A2SET.cfg | grep rsvp >> A2SET.LDP-RSVP
+cat A2SET.cfg | grep RSVP >> A2SET.LDP-RSVP
+
+
+touch A3SET.LDP-RSVP
+cat A3SET.cfg | grep ldp > A3SET.LDP-RSVP
+cat A3SET.cfg | grep LDP >> A3SET.LDP-RSVP
+cat A3SET.cfg | grep rsvp >> A3SET.LDP-RSVP
+cat A3SET.cfg | grep RSVP >> A3SET.LDP-RSVP
+
+
+touch B2SET.LDP-RSVP
+cat B2SET.cfg | grep ldp > B2SET.LDP-RSVP
+cat B2SET.cfg | grep LDP >> B2SET.LDP-RSVP
+cat B2SET.cfg | grep rsvp >> B2SET.LDP-RSVP
+cat B2SET.cfg | grep RSVP >> B2SET.LDP-RSVP
+
+
+touch C2SET.LDP-RSVP
+cat C2SET.cfg | grep ldp > C2SET.LDP-RSVP
+cat C2SET.cfg | grep LDP >> C2SET.LDP-RSVP
+cat C2SET.cfg | grep rsvp >> C2SET.LDP-RSVP
+cat C2SET.cfg | grep RSVP >> C2SET.LDP-RSVP
+
+
+touch D2SET.LDP-RSVP
+cat D2SET.cfg | grep ldp > D2SET.LDP-RSVP
+cat D2SET.cfg | grep LDP >> D2SET.LDP-RSVP
+cat D2SET.cfg | grep rsvp >> D2SET.LDP-RSVP
+cat D2SET.cfg | grep RSVP >> D2SET.LDP-RSVP
+
+
+touch E2SET.LDP-RSVP
+cat E2SET.cfg | grep ldp > E2SET.LDP-RSVP
+cat E2SET.cfg | grep LDP >> E2SET.LDP-RSVP
+cat E2SET.cfg | grep rsvp >> E2SET.LDP-RSVP
+cat E2SET.cfg | grep RSVP >> E2SET.LDP-RSVP
+
+touch F2SET.LDP-RSVP
+cat F2SET.cfg | grep ldp > F2SET.LDP-RSVP
+cat F2SET.cfg | grep LDP >> F2SET.LDP-RSVP
+cat F2SET.cfg | grep rsvp >> F2SET.LDP-RSVP
+cat F2SET.cfg | grep RSVP >> F2SET.LDP-RSVP
+
+
+touch G2SET.LDP-RSVP
+cat G2SET.cfg | grep ldp > G2SET.LDP-RSVP
+cat G2SET.cfg | grep LDP >> G2SET.LDP-RSVP
+cat G2SET.cfg | grep rsvp >> G2SET.LDP-RSVP
+cat G2SET.cfg | grep RSVP >> G2SET.LDP-RSVP
+
+
+touch H2SET.LDP-RSVP
+cat H2SET.cfg | grep ldp > H2SET.LDP-RSVP
+cat H2SET.cfg | grep LDP >> H2SET.LDP-RSVP
+cat H2SET.cfg | grep rsvp >> H2SET.LDP-RSVP
+cat H2SET.cfg | grep RSVP >> H2SET.LDP-RSVP
+
+
+touch I2SET.LDP-RSVP
+cat I2SET.cfg | grep ldp > I2SET.LDP-RSVP
+cat I2SET.cfg | grep LDP >> I2SET.LDP-RSVP
+cat I2SET.cfg | grep rsvp >> I2SET.LDP-RSVP
+cat I2SET.cfg | grep RSVP >> I2SET.LDP-RSVP
+
+
+
+touch J2SET.LDP-RSVP
+cat J2SET.cfg | grep ldp > J2SET.LDP-RSVP
+cat J2SET.cfg | grep LDP >> J2SET.LDP-RSVP
+cat J2SET.cfg | grep rsvp >> J2SET.LDP-RSVP
+cat J2SET.cfg | grep RSVP >> J2SET.LDP-RSVP
+
+
